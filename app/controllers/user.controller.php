2a9526c4edd15d51015d886b35043e76a669357b
Sergiu Pirlici <sergiu103@mail.ru>
2015-08-05 17:45:04 +0300

    .

---
+++ app/controllers/user.controller.php
@@ -0,0 +1,44 @@
+<?php 
+
+/**
+ * User controller
+ * 
+ * @author  [Sergiu Pirlici](www.spirlici.com)
+ */
+class UserController extends Controller {
+    
+    /**
+     * Default method
+     * 
+     * Redirects to /user/login if user is not logged in and to /user/profile if user is logged in
+     */
+    public function index(){
+        if($this->user->is_log()) {
+            redirect('/user/profile');
+        }
+        else {
+            redirect('/user/login');
+        }
+    }
+    
+    /**
+     * Display login form
+     */
+    public function login(){
+        
+    }
+    
+    /**
+     * Displays signup form
+     */
+    public function signup(){
+        
+    }
+    
+    /**
+     * Show user profile
+     */
+    public function profile(){
+        
+    }
+}
